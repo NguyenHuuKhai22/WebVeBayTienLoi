9de06eee5ba595318073842996d09715771cc0cc
Nguyen Huu Khai <nguyenhuukhai22052003@gmail.com>
2025-04-13 22:32:15 +0700

    Xóa thư mục New folder/DoAn và các file liên quan

--- New folder/DoAn/Do_AN/resources/views/emails/test.blade.php
+++
@@ -1,2 +0,0 @@
-<h2>Test Email</h2>
-<p>Nếu bạn nhận được email này, hệ thống gửi mail đã hoạt động!</p>
--- New folder/DoAn/Do_AN/resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- resources/views/partials/footer.blade.php -->
-<footer class="bg-gray-100 py-4">
-    <div class="container mx-auto text-center">
-        <p class="text-gray-600">© 2025 {{ __('Vietnam Airlines. All rights reserved.') }}</p>
-    </div>
-</footer>
